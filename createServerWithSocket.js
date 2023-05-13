d77ad21fc656c2b0ac40abc901ce6af9cd1bd404
sami12344 <freelancersami16203@gmail.com>
2023-05-13 16:41:02 +0600

    createServerWithSocket

--- createServerWithSocket.js
+++ createServerWithSocket.js
@@ -1,9 +1,20 @@
+const { log } = require('console')
 const express = require('express')
 const app = express()
 const http = require('http')
-const {Server} = require('socket.io')
+const { Server } = require('socket.io')
 const expressServer = http.createServer(app)
 const io = new Server(expressServer)
+
+
+
+io.on('connection', (socket) => {
+  console.log('New user connected')
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+})
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
